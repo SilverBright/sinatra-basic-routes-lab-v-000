44d6cb890037f6367372e8a8d27fa9237473d910
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-18 22:41:46 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,10 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/name' do
-    "My name is Babs"
+    "My name is Babs."
   end
 
   get '/hometown' do
+    "My hometown is San Mateo, CA."
   end
 
 end
